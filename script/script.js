2dc88ca08103ad3634b9e50197e51979e6450372
Paméla <pamela.schmidinger@zhdk.ch>
2019-05-19 13:29:17 +0200

    adding list and 2 buttons
    event that button adds element to list

--- script/script.js
+++ script/script.js
@@ -1,9 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#fadeButton').click(function(event) {
+    $('#fadeButton').click(function (event) {
         console.log('Fade out that div! 👻');
         console.log('event.target: ', event.target);
-      });
+    });
+
+    $('#addProduct1ToCart').click(function (event) {
+        console.log('add to cart');
+        var container = $('.js-add-content');
+        var textForContent = 'This can contain more than just one element';
+
+        var newHTMLContent = createListItem(textForContent);
+        
+        console.log('newHTMLContent: ', newHTMLContent);
+        
+        // And add the newly created HTML string as the last part of our container
+        container.append(newHTMLContent);
+        console.log(container);
+    });
+
+    $('#addProduct2ToCart').click(function (event) {
+        console.log('add to cart');
+        
+    });
+
+  // Function to create content HTML string
+  function createListItem(text) {
+    return `
+    <li>${text}</li>
+    `
+  }
+
 
 
 });
